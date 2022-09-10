c2c6bff0e157ec31ebf2cfd554cda483b3842e6a
Tucno0 <yantucnoperez@gmail.com>
2022-09-09 19:08:37 -0500

    Agregando las preferencias del sistema

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    // Boton dark mode
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     });
+
 }
 
 function eventListeners() {
