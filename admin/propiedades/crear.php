5ba6b003b6d914394556bbb6fe88de7b73ff68e1
Tucno0 <yantucnoperez@gmail.com>
2022-09-11 23:01:43 -0500

    Validando el formulario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,55 +1,130 @@
 <?php
-    require '../../includes/funciones.php';
-    incluirTemplate('header');
+
+// Base de Datos
+require '../../includes/config/database.php';
+$db = conectarDB();
+
+// Arreglo con mensaje de errores
+$errores = [];
+
+// Ejecutar el codigo despues de que el usuario envia el formulario
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    $titulo = $_POST['titulo'];
+    $precio = $_POST['precio'];
+    $descripcion = $_POST['descripcion'];
+    $habitaciones = $_POST['habitaciones'];
+    $wc = $_POST['wc'];
+    $estacionamiento = $_POST['estacionamiento'];
+    $vendedores_id = $_POST['vendedor'];
+
+    if (!$titulo) {
+        $errores[] = "Debes añadir un titulo";
+    }
+
+    if (!$precio) {
+        $errores[] = "El precio es obligatorio";
+    }
+
+    if (strlen($descripcion) < 50) {
+        $errores[] = "La descripcion es obligatorio y debe tener al menos 50 caracteres";
+    }
+
+    if (!$wc) {
+        $errores[] = "El numero de baños es obligatorio";
+    }
+
+    if (!$estacionamiento) {
+        $errores[] = "El numero de lugares de estacionamiento es obligatorio";
+    }
+
+    if (!$vendedores_id) {
+        $errores[] = "Elige un vendedor";
+    }
+
+
+    // echo "<pre>";
+    // var_dump($errores);
+    // echo "</pre>";
+
+    // Revisar que el arreglo de errores este vacio
+    if (empty($errores)) {
+        // Insertar en la base de datos
+        $query = "INSERT INTO propiedades(titulo, precio, descripcion, habitaciones, wc, estacionamiento ,vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedores_id')";
+
+        // echo $query;
+        $resultado = mysqli_query($db, $query);
+
+        if (!$resultado) {
+            echo "insertado correctamente";
+        }
+    }
+}
+
+// Header
+require '../../includes/funciones.php';
+incluirTemplate('header');
+
 ?>
-    <main class="contenedor seccion">
-        <h1>Crear</h1>
+<main class="contenedor seccion">
+    <h1>Crear</h1>
 
-        <a href="/admin" class="boton boton-verde">Volver</a>
+    <a href="/admin" class="boton boton-verde">Volver</a>
 
-        <form class="formulario">
-            <fieldset>
-                <legend>Informacion General</legend>
+    <?php foreach ($errores as $error ): ?>
+        <div class="alerta error">
+            <?php echo $error;?>
 
-                <label for="titulo">Titulo:</label>
-                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+        </div>
+    <?php endforeach; ?>
 
-                <label for="precio">Precio:</label>
-                <input type="number" id="precio" placeholder="Precio Propiedad">
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+        <fieldset>
+            <legend>Informacion General</legend>
 
-                <label for="imagen">Imagen:</label>
-                <input type="file" id="imagen" accept="image/jpeg, image/png">
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
 
-                <label for="descripcion">Descripcion:</label>
-                <textarea id="descripcion"></textarea>
-            </fieldset>
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
 
-            <fieldset>
-                <legend>Informacio de la propiedad</legend>
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
 
-                <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
-                
-                <label for="wc">Baños:</label>
-                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+            <label for="descripcion">Descripcion:</label>
+            <textarea id="descripcion" name="descripcion"></textarea>
+        </fieldset>
 
-                <label for="estacionamiento">Estacionamiento:</label>
-                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
-            </fieldset>
+        <fieldset>
+            <legend>Informacio de la propiedad</legend>
 
-            <fieldset>
-                <legend>Vendedor</legend>
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
 
-                <select>
-                    <option value="1">Jhampier</option>
-                    <option value="2">Karen</option>    
-                </select>
-            </fieldset>
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
 
-            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
-        </form>
-    </main>
+            <label for="estacionamiento">Estacionamiento:</label>
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="vendedor">
+                <option value="">-- Seleccione --</option>
+                <option value="1">Jhampier</option>
+                <option value="2">Karen</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
+</main>
 
 <?php
-    incluirTemplate('footer');
+incluirTemplate('footer');
 ?>
